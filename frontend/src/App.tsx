ee14ec6b8bdbf7abcf43daf47a7c429288d7b762
Jules Jean-Louis <114387374+jules-jean-louis1@users.noreply.github.com>
2026-09-11 11:18:36 +0200

    Feat k8s/service acount and toggle mode for react (#7)
    * chore: delete old files
    
    * feat: improve pipeline
    
    * feat: add Kubernetes RBAC configuration and update deployment workflow
    
    * feat: add night mode
    
    * feat: update permissions in CI workflow and change frontend title
    
    * refactor: standardize formatting and improve readability in theme and dropdown components
    
    * fix: correct syntax for Playwright browser installation in CI workflow
    
    * fix: update Playwright browser installation and improve end-to-end test command

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./router";
+import { ThemeProvider } from "./components/theme/ThemeProvider";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
