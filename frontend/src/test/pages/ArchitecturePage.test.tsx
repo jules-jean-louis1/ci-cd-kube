ea58b72d2803dd4029d4198d7a4d015559bee213
jules-jl <jules.jeanlouis13@gmail.com>
2026-09-13 23:38:18 +0200

    Add tests for ArchitecturePage component

---
+++ frontend/src/test/pages/ArchitecturePage.test.tsx
@@ -0,0 +1,13 @@
+import { ArchitecturePage } from "@/pages/ArchitecturePage";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+
+describe("Architecture page", () => {
+    it("Présente l'architerture", () => {
+        render(<ArchitecturePage />);
+
+        expect(
+            screen.getByRole("heading", { name: "Du commit à l’application en ligne" }),
+        ).toBeInTheDocument();
+    })
+})
