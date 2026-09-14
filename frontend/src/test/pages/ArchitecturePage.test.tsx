391ce212b5b09f3a82ac33f1fddfada47a538127
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2026-09-14 08:55:07 +0200

    Refactor ArchitecturePage test formatting and update docker-compose volumes

--- frontend/src/test/pages/ArchitecturePage.test.tsx
+++ frontend/src/test/pages/ArchitecturePage.test.tsx
@@ -3,11 +3,11 @@ import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 
 describe("Architecture page", () => {
-    it("Présente l'architerture", () => {
-        render(<ArchitecturePage />);
+  it("Présente l'architerture", () => {
+    render(<ArchitecturePage />);
 
-        expect(
-            screen.getByRole("heading", { name: "Du commit à l’application en ligne" }),
-        ).toBeInTheDocument();
-    })
-})
+    expect(
+      screen.getByRole("heading", { name: "Du commit à l’application en ligne" }),
+    ).toBeInTheDocument();
+  });
+});
